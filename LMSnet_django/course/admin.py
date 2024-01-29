f46a8b3224f7237a0a0a7d110e775892ebbabda1
MinatoNami <minato.nami.dev@gmail.com>
2024-01-29 10:01:17 +0800

    Add plural name for category and configured public api endpoints

--- LMSnet_django/course/admin.py
+++ LMSnet_django/course/admin.py
@@ -2,7 +2,17 @@ from django.contrib import admin
 
 from .models import Category, Course, Lesson, Comment
 
+class LessonCommentInline(admin.TabularInline):
+    model = Comment
+    extra = 0
+
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ['title', 'course', 'status', 'lesson_type']
+    list_filter = ['status', 'lesson_type']
+    search_fields = ['title', 'short_description', 'long_description']
+    inlines = [LessonCommentInline]
+
 admin.site.register(Category)
 admin.site.register(Course)
-admin.site.register(Lesson)
+admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Comment)
