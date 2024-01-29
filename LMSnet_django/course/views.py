ac3e603899900f8e0d740a960617ba7f05951384
Lionel <txg_ace@photons.com.tw>
2024-01-29 10:01:17 +0800

    Add plural name for category and configured public api endpoints

--- LMSnet_django/course/views.py
+++ LMSnet_django/course/views.py
@@ -1,19 +1,22 @@
 from django.shortcuts import render
 
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
 
 from .models import Course, Lesson, Comment, Category
 from .serializers import CategoryListSerializer, CourseListSerializer, CourseSerializer, LessonListSerializer, CommentListSerializer
 
 @api_view(['GET'])
+@authentication_classes([])
+@permission_classes([])
 def get_categories(request):
     categories = Category.objects.all()
     serializer = CategoryListSerializer(categories, many=True)
     return Response(serializer.data)
 
 @api_view(['GET'])
+@authentication_classes([])
+@permission_classes([])
 def get_courses(request):
     category_id = request.GET.get('category_id', '')
     courses = Course.objects.all()
@@ -24,24 +27,34 @@ def get_courses(request):
     return Response(serializer.data)
 
 @api_view(['GET'])
+@authentication_classes([])
+@permission_classes([])
 def get_frontpage_courses(request):
     courses = Course.objects.all()[0:4]
     serializer = CourseListSerializer(courses, many=True)
     return Response(serializer.data)
 
 @api_view(['GET'])
+@authentication_classes([])
+@permission_classes([])
 def get_course(request, slug):
     course = Course.objects.get(slug=slug)
     course_serializer = CourseSerializer(course)
     lesson_serializer = LessonListSerializer(course.lessons.all(), many=True)
 
+    if (request.user.is_authenticated):
+        course_data = course_serializer.data
+    else:
+        course_data ={}
     data = {
-        'course': course_serializer.data,
+        'course': course_data,
         'lessons': lesson_serializer.data
     }
     return Response(data)
 
 @api_view(['GET'])
+@authentication_classes([])
+@permission_classes([])
 def get_comments(request, course_slug, lesson_slug):
     lesson = Lesson.objects.get(slug=lesson_slug)
     serializer = CommentListSerializer(lesson.comments.all(), many=True)
